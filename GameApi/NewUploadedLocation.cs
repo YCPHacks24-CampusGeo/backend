b88af438223dae5f3eb26f674f5a59a94f288c00
UnicycleUnicorn <brand227796@gmail.com>
2024-11-01 02:39:35 -0400

    Please work

--- GameApi/NewUploadedLocation.cs
+++ GameApi/NewUploadedLocation.cs
@@ -2,7 +2,8 @@
 
 public class NewUploadedLocation
 {
-    public string Name { get; set; }
+    // public string Name { get; set; }
     public GeoLocation Location { get; set; }
-    public LocationTag[] locationTags { get; set; }
+    // public LocationTag[] locationTags { get; set; }
+    public string base64JGP {  get; set; }
 }
