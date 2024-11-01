007df83a691f7a583d401fa7d9726ca5b56c4b5c
UnicycleUnicorn <brand227796@gmail.com>
2024-11-01 13:59:05 -0400

    Create file

--- GameApi/Controllers/LocationUploadController.cs
+++ GameApi/Controllers/LocationUploadController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text.Json;
 
 namespace GameApi.Controllers;
 [Route("[controller]/[action]")]
@@ -12,5 +14,12 @@ public class LocationUploadController : ControllerBase
         Console.WriteLine(uploadedLocation.Location.Latitude);
         Console.WriteLine(uploadedLocation.Location.Longitude);
         Console.WriteLine(uploadedLocation.base64JPG[0..100]);
+
+        string jsonString = JsonSerializer.Serialize(uploadedLocation);
+
+        string path = $"/uploaded_locations/{Path.GetRandomFileName()}.json";
+
+        using FileStream fs = System.IO.File.Create(path);
+        System.IO.File.WriteAllText(path, jsonString);
     }
 }
