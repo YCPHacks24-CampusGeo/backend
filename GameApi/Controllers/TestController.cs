070d2e8a6cbf1b45660562f488736187fe7c5343
UnicycleUnicorn <brand227796@gmail.com>
2024-11-02 09:32:45 -0400

    update test controller

--- GameApi/Controllers/TestController.cs
+++ GameApi/Controllers/TestController.cs
@@ -1,6 +1,7 @@
 ﻿using GameApi.Objects;
 using GameApi.Utils;
 using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
 using System.Text.Json;
 
 namespace GameApi.Controllers;
@@ -15,4 +16,18 @@ public class TestController : ControllerBase
         int i = r.Next(GameObjects.Locations.Count);
         return Ok(GameObjects.Locations[i]);
     }
+
+    [HttpGet]
+    public IActionResult GetIcon()
+    {
+        Random r = new Random();
+        int i = r.Next(GameObjects.Markers.Count);
+
+        return Ok(new
+        {
+            Icon = GameObjects.Markers[i],
+            Name = Generators.GenerateName(),
+        });
+    }
+
 }
