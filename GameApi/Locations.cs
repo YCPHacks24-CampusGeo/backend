978d4d5ac75f5e2f6ec5c1d56390afce0348cf5e
UnicycleUnicorn <brand227796@gmail.com>
2024-11-01 22:22:56 -0400

    Initial location and removal of WeatherForecast stuffs.

---
+++ GameApi/Locations.cs
@@ -0,0 +1,20 @@
+﻿namespace GameApi;
+
+public static class Locations
+{
+    public static List<Location> List = [
+        new Location(new GeoLocation(39.947324218329115, -76.73041865229607), "qxa3zrrx.rvw"),
+    ];
+}
+
+public class Location
+{
+    public GeoLocation GeoLocation { get; set; }
+    public string ImageKey;
+
+    public Location(GeoLocation location, string imagekey)
+    {
+        GeoLocation = location;
+        ImageKey = imagekey;
+    }
+}
